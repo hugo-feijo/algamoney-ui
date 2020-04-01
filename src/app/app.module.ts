70d939566dd66231dbd62765e9a2fcf648dcb12c
Hugo Feijó <hugo.ofeijo@gmail.com>
2020-04-01 12:31:41 -0400

    Criado o modulo Core com a navBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
-import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { NavbarComponent } from './navbar/navbar.component';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 
@@ -13,14 +12,14 @@ import { PessoasModule } from './pessoas/pessoas.module';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
 
     LancamentosModule,
-    PessoasModule
+    PessoasModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
